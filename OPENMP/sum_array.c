476cca96212087ba9a93e35ba8270111eca45475
lojayn2004 <111882914+lojayn2004@users.noreply.github.com>
2025-06-07 22:23:21 +0300

    Update sum_array.c

--- OPENMP/sum_array.c
+++ OPENMP/sum_array.c
@@ -5,10 +5,18 @@ int main() {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int arrLen = sizeof(arr) / sizeof(arr[0]);
     int arrSum = 0;
+   
+    
     int i = 0;
     /*
-    *  The reduction clause in OpenMP is used to safely perform parallel accumulation operations like: +, -, *, &, |, ^, && and ||, min, max
-    *  It helps us prevent race conditions
+    *  (reduction(+:arrSum)) 
+    *          The reduction clause in OpenMP is used to safely perform parallel 
+    *          accumulation operations like: +, -, *, &, |, ^, && and ||, min, max
+    *          It helps us prevent race conditions
+    *
+    *  (private(i)) 
+    *          Each thread gets its own copy of i but with a different initial value 
+    *          so we could loop on different parts of the array 
     */
     #pragma omp parallel for private(i) reduction(+:arrSum)
     for(i = 0; i < arrLen; i++) {
