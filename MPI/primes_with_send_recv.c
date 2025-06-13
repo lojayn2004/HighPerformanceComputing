78bfadf98e7c0902f81e7361863b383657d22f7a
lojayn2004 <111882914+lojayn2004@users.noreply.github.com>
2025-06-13 12:44:42 +0300

    Update primes_with_send_recv.c

--- MPI/primes_with_send_recv.c
+++ MPI/primes_with_send_recv.c
@@ -21,13 +21,17 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
     MPI_Comm_size(MPI_COMM_WORLD, &size); 
     if(rank == 0) {
+       // root handles user input 
        printf("Enter the lower bound: ");
        fflush(stdout); 
        scanf("%d", &st);
        printf("Enter the upper bound: ");
        fflush(stdout); 
        scanf("%d", &e);
+      
        double start = MPI_Wtime();
+       // Root counts primes in  its range 
+      
        int eachProcessRange = (e - st + 1) / size; 
        int rem = (e - st + 1) % size;
        int p_st = st + rank * eachProcessRange + (rank < rem ? rank: rem);
@@ -39,33 +43,43 @@ int main(int argc, char** argv) {
        }
        printf("P(%d): Total Number of primes from %d to %d is:  %d \n", rank, p_st, p_e - 1, cur_cnt);
        total_primes = cur_cnt;
+
+       // Root sends the start & end to each other process so each process could calculate its range
        for(int i = 1;i < size;i++) {
           MPI_Send(&st, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
           MPI_Send(&e, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
        }
+
+       // After each process calculates its sum the root recives that sum & adds it to the global sum 
        for(int i = 1;i < size;i++) {
           int part_sum = 0;
           MPI_Recv(&part_sum, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           total_primes += part_sum;
        
        }
+      
        double end = MPI_Wtime();
        
        printf("Total Number of primes from %d to %d is:  %d \n", st, e, total_primes);
        printf("Total Time needed %f \n", end - start);
     }
     else  {
+          // Each process recieves the start & the end from the root to calculate the range 
+          // over which it will count the primes 
           MPI_Recv(&st, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           MPI_Recv(&e, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      
           // range of numbers / size of all processes - 1
           // Determine the range of elements for each process.
           // If the process rank is less than the remainder, it gets one extra element.
           // This distributes the remaining elements evenly across processes,  
           // preventing the root process from handling all the extra work.
+      
           int eachProcessRange = (e - st + 1) / size; 
           int rem = (e - st + 1) % size;
           int p_st = st + rank * eachProcessRange + (rank < rem ? rank : rem);
           int p_e = p_st + eachProcessRange + (rank < rem ? 1: 0);
+      
           int cur_cnt = 0;
           for(int i = p_st;i < p_e;i++) {
             if(IsPrime(i)) 
